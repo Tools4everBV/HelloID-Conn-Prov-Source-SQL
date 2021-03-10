2b18245877ec5831fec44c940933a07c32344a92
rvddijssel <67150235+rvddijssel@users.noreply.github.com>
2021-03-10 10:12:46 +0100

    added configuration params in importing departments

--- departments.ps1
+++ departments.ps1
@@ -1,4 +1,4 @@
-function GetDataFromSqlDatabase($Query, $ConnectionString) {
+function PerfromQuery($Query, $ConnectionString) {
     try {
         # Initialize connection and query information
         # Connect to the SQL server
@@ -8,33 +8,29 @@ function GetDataFromSqlDatabase($Query, $ConnectionString) {
         $SqlCmd.Connection = $SqlConnection;
         $SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter;
         
+        #Query to get all person information adjust to liking#
         $SqlCmd.CommandText = $query;
         $SqlAdapter.SelectCommand = $SqlCmd;
         
         $DataSet = New-Object System.Data.DataSet;
         $SqlAdapter.Fill($DataSet) | out-null;
-        return $DataSet.Tables[0] | Select-Object -Property * -ExcludeProperty RowError, RowState, Table, ItemArray, HasErrors;
+        $sqlData = $DataSet.Tables[0];
+        return $sqlData | Select-Object -Property * -ExcludeProperty RowError, RowState, Table, ItemArray, HasErrors;
     }
     catch {
         Write-Error "Something went wrong while connecting to the SQL server";
         Write-Error $_.Exception.Message;
         Exit;
     }
-    finally 
-    {
-        if($SqlConnection -and $SqlConnection.State -eq [System.Data.ConnectionState]::Open)
-        {
-            $SqlConnection.Close()
-        }
-    }
 }
 
-$server = ""
-$database = ""
-$userId = ""
-$password = ""
+$config = ConvertFrom-Json $configuration
+$server = $config.server
+$database = $config.database
+$userId = $config.user
+$password = $config.password
 $connectionString = "Data Source=$server;Initial Catalog=$database;User Id=$userId;Password=$password;";
-$languageId = 2;
+$languageId = "(1,2)"; # 1=NL, 2=EN
 
 #Query to get all department information adjust to liking#
 $departmentsQuery = "SELECT [Organisatorische_eenheid] as [ExternalId]
@@ -42,9 +38,9 @@ $departmentsQuery = "SELECT [Organisatorische_eenheid] as [ExternalId]
                             ,[Omschrijving] as [Name]
                             ,[Leidinggevende] as [ManagerId]
                             ,[Parent] as [ParentDepartmentId]
-                    FROM [dbo].[T4E_IAM_OrganizationalUnits] WHERE [lang_id] = $languageId"
+                    FROM [dbo].[T4E_IAM_OrganizationalUnits] WHERE [lang_id] in $languageId"
 
-$departments = GetDataFromSqlDatabase -Query $departmentsQuery -ConnectionString $connectionString
+$departments = PerfromQuery -Query $departmentsQuery -ConnectionString $connectionString
     
 Foreach ($department in $departments) {
     Write-Output $department | ConvertTo-Json -Depth 10;
